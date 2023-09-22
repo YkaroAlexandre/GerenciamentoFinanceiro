233d3b59266dee39eee40fbcfcd5cb9415bdf170
unknown <ykaroalexandre@hotmail.com>
2023-09-21 22:57:24 -0300

    Commiting

--- src/main/java/com/pweb/aplication/GerenciadorFinanceiro/repositories/LoginRepository.java
+++ src/main/java/com/pweb/aplication/GerenciadorFinanceiro/repositories/LoginRepository.java
@@ -3,8 +3,6 @@ package com.pweb.aplication.GerenciadorFinanceiro.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.pweb.aplication.GerenciadorFinanceiro.models.Login;
-
-
 public interface LoginRepository extends CrudRepository<Login , Long>{
     
 }
